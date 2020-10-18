82184605bfd918f78294e667bf3782569ff0c159
Ola Ouso <ouso.ola@gmail.com>
2020-10-18 18:05:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+            .then(function(response) {
+                return response.json()
+            })
+            .then(json => {
+                document.querySelector("body").innerHTML+= Object.entries(json)
+            })
+
+            .catch(function(error){
+                document.querySelector("body").innerHTML+=`<p>${error.message}</p>`
+
+            });
+    }
+
+    submitData("dcd","sdjhas") 
